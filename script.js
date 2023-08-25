e45f64ddc327f449ca33c344933bf51f30e58c5a
Tanmay jain <134151751+Tanmayjain0906@users.noreply.github.com>
2023-08-25 12:55:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let output = document.getElementById("output");
+
+let pro = new Promise((resolve) => {
+   setTimeOut(() => {
+	   output.innerText = "Hello, world";
+   }, 1000)
+})
